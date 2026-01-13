bfe4d33907a08ab2d8efc7e97c0f8be6f75b2a11
salman831547 <python831547@gmail.com>
2026-01-13 16:26:46 +0530

    CORS added in the fronend and terraform main.tf updated to handle CORS

--- consumer.py
+++ consumer.py
@@ -3,22 +3,28 @@ import boto3
 import os
 import uuid
 
-dynamodb = boto3.resource('dynamodb')
-TABLE_NAME = os.environ['DYNAMODB_TABLE']
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = os.environ["DYNAMODB_TABLE"]
 table = dynamodb.Table(TABLE_NAME)
 
+
 def lambda_handler(event, context):
-    for record in event['Records']:
+    for record in event["Records"]:
         # 1. Get message body from SQS
-        payload = json.loads(record['body'])
-        
+        payload = json.loads(record["body"])
+
+        # --- CHAOS MODE: SIMULATE FAILURE ---
+        if payload.get("product") == "Poison":
+            raise Exception("This order is poisonous!")
+        # ------------------------------------
+
         # 2. Prepare item for DynamoDB (Add a unique ID)
         item = {
-            'OrderId': str(uuid.uuid4()),
-            'Product': payload.get('product', 'Unknown'),
-            'Quantity': payload.get('quantity', 1)
+            "OrderId": str(uuid.uuid4()),
+            "Product": payload.get("product", "Unknown"),
+            "Quantity": payload.get("quantity", 1),
         }
-        
+
         # 3. Write to DB
         table.put_item(Item=item)
         print(f"Order processed: {item}")
